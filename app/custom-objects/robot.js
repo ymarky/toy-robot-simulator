6398d30d23e1020bbeb3cce89b88a33d50af6cbb
Mark Yost <mark.yost@gmail.com>
2019-02-01 15:35:28 -0500

    Progress on the robot object

--- app/custom-objects/robot.js
+++ app/custom-objects/robot.js
@@ -1,9 +1,99 @@
 import EmberObject from '@ember/object';
 
-const Robot = EmberObject.extend({
-  say(thing) {
-    return thing;
+function translateFacingIntToString(facingInt) {
+  switch (facingInt) {
+    case 3:
+      return 'WEST';
+    case 2:
+      return 'SOUTH';
+    case 1:
+      return 'EAST';
+    case 0:
+      return 'NORTH';
+    default:
+      return 'UNDEFINED';
   }
+}
+
+function translateFacingStringToInt(facingString) {
+  switch (facingString) {
+    case 'WEST':
+      return 3;
+    case 'SOUTH':
+      return 2;
+    case 'EAST':
+      return 1;
+    default: // covers the 'NORTH' case
+      return 0;
+  }
+}
+
+function facingStringIsValid(facingString) {
+  return facingString == 'NORTH'
+    || facingString == 'EAST'
+    || facingString == 'WEST'
+    || facingString == 'SOUTH';
+}
+
+function coordinateIsValid(coordinate) {
+  return coordinate >= 0 && coordinate <=4;
+}
+
+function turnLeft(currentFacing) {
+  return currentFacing <= 0
+    ? 3
+    : currentFacing - 1;
+}
+
+function turnRight(currentFacing) {
+  return currentFacing >= 3
+    ? 0
+    : currentFacing + 1;
+}
+
+const Robot = EmberObject.extend({
+  init() {
+    this.unplace();
+  },
+
+  unplace() {
+    this.set('isOnTable', false);
+    this.set('xCoordinate', undefined);
+    this.set('yCoordinate', undefined);
+    this.set('facing', undefined);
+  },
+
+  place(xCoordinate, yCoordinate, facingString) {
+    if(!coordinateIsValid(xCoordinate)) return;
+    if(!coordinateIsValid(yCoordinate)) return;
+    if(!facingStringIsValid(facingString)) return;
+
+    this.set('isOnTable', true);
+    this.set('xCoordinate', xCoordinate);
+    this.set('yCoordinate', yCoordinate);
+    this.set('facing', translateFacingStringToInt(facingString));
+  },
+
+  turnLeft() {
+    if (!this.isOnTable) return;
+
+    this.set('facing', turnLeft(this.facing));
+  },
+
+  turnRight() {
+    if (!this.isOnTable) return;
+
+    this.set('facing', turnRight(this.facing));
+  },
+
+  report() {
+    return {
+      isOnTable: this.isOnTable,
+      xCoordinate: this.xCoordinate,
+      yCoordinate: this.yCoordinate,
+      facing: translateFacingIntToString(this.facing),
+    };
+  },
 });
 
 export default Robot;
--- tests/unit/custom-objects/robot-test.js
+++ tests/unit/custom-objects/robot-test.js
@@ -12,14 +12,101 @@ module('Unit | Custom Objects | Robot', function() {
     assert.ok(result);
   });
 
-  test('say', function(assert) {
+  test('report - unplaced', function(assert) {
     // Arrange
     const robot = Robot.create();
 
     // Act
-    const result = robot.say('test');
+    const result = robot.report();
 
     // Assert
-    assert.equal(result, 'test');
+    assert.equal(result.isOnTable, false);
+    assert.equal(result.xCoordinate, undefined);
+    assert.equal(result.yCoordinate, undefined);
+    assert.equal(result.facing, 'UNDEFINED');
+  });
+
+  test('left - unplaced', function(assert) {
+    // Arrange
+    const robot = Robot.create();
+
+    // Act
+    robot.turnLeft();
+
+    // Assert
+    const result = robot.report();
+    assert.equal(result.isOnTable, false);
+    assert.equal(result.xCoordinate, undefined);
+    assert.equal(result.yCoordinate, undefined);
+    assert.equal(result.facing, 'UNDEFINED');
+  });
+
+  test('right - unplaced', function(assert) {
+    // Arrange
+    const robot = Robot.create();
+
+    // Act
+    robot.turnRight();
+
+    // Assert
+    const result = robot.report();
+    assert.equal(result.isOnTable, false);
+    assert.equal(result.xCoordinate, undefined);
+    assert.equal(result.yCoordinate, undefined);
+    assert.equal(result.facing, 'UNDEFINED');
+  });
+
+  test('place - bad x coord', function(assert) {
+    // Arrange
+    const robot = Robot.create();
+    const x = 'mark';
+    const y = 1;
+    const f = 'NORTH';
+
+    // Act
+    robot.place(x, y, f);
+
+    // Assert
+    const result = robot.report();
+    assert.equal(result.isOnTable, false);
+    assert.equal(result.xCoordinate, undefined);
+    assert.equal(result.yCoordinate, undefined);
+    assert.equal(result.facing, 'UNDEFINED');
+  });
+
+  test('place - bad y coord', function(assert) {
+    // Arrange
+    const robot = Robot.create();
+    const x = 1;
+    const y = 'mark';
+    const f = 'NORTH';
+
+    // Act
+    robot.place(x, y, f);
+
+    // Assert
+    const result = robot.report();
+    assert.equal(result.isOnTable, false);
+    assert.equal(result.xCoordinate, undefined);
+    assert.equal(result.yCoordinate, undefined);
+    assert.equal(result.facing, 'UNDEFINED');
+  });
+
+  test('place - bad facing', function(assert) {
+    // Arrange
+    const robot = Robot.create();
+    const x = 1;
+    const y = 1;
+    const f = 'mark';
+
+    // Act
+    robot.place(x, y, f);
+
+    // Assert
+    const result = robot.report();
+    assert.equal(result.isOnTable, false);
+    assert.equal(result.xCoordinate, undefined);
+    assert.equal(result.yCoordinate, undefined);
+    assert.equal(result.facing, 'UNDEFINED');
   });
 });

42be19173bba0e84cce011093f4634342244c70e
Mark Yost <mark.yost@gmail.com>
2019-02-01 20:16:39 -0500

    Lots of progress, almost done

--- app/controllers/robot.js
+++ app/controllers/robot.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 import RobotCommandParser from 'toy-robot-simulator/custom-libraries/robot-command-parser';
-// import Robot from 'toy-robot-simulator/custom-objects/robot';
+import Robot from 'toy-robot-simulator/custom-objects/robot';
 
 /*
 Some test input
@@ -24,30 +24,57 @@ place 2,-1,north
 place 2,5,north
 place 2,3,north,invalid
 place 2,3,north invalid
+
+place 2,3,west
+report
+move
+report
 */
+
+// This probably belongs to the robot
+function executeCommandOnRobot(robot, command) {
+  switch (command.command) {
+    case 'MOVE':
+      return robot.move();
+    case 'LEFT':
+      return robot.turnLeft();
+    case 'RIGHT':
+      return robot.turnRight();
+    case 'REPORT':
+      return robot.report();
+    case 'PLACE':
+      return robot.place(command.xCoordinate, command.yCoordinate, command.facing);
+  }
+}
 
 export default Controller.extend({
   textInput: '',
-  textOutput: '',
+  results: undefined,
 
   actions: {
     clearInput() {
       this.set('textInput', '');
     },
     parseInput() {
-      console.log(`parseInput!`);
-      console.log(this.textInput);
-
-      // Parse textInput into an array of commands
       const commands = RobotCommandParser.parseTextInputIntoCommands(this.textInput);
 
       if (!commands.length || commands.length == 0) return;
 
-      console.log(`Got some commands!`);
-      console.log(commands);
+      const robot = Robot.create();
+      const results = [];
 
-      // Create a new robot object
-      // Execute our commands on the robot object
+      commands.forEach((c) => {
+        console.log(c);
+        const result = executeCommandOnRobot(robot, c);
+        console.log(robot);
+
+        if(c.command == 'REPORT') {
+          console.log(result);
+          results.push(result);
+        }
+      });
+
+      this.set('results', results);
     },
   },
 });
